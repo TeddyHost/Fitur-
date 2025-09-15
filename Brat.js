5bfdd048ae67c32612e4892dff91eaeac9584029
TeddyHost <teddydermawan218@gmail.com>
2025-09-15 14:22:54 +0700

    Update Brat.js

--- Brat.js
+++ Brat.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 module.exports = (bot) => {
   bot.command('brat', async (ctx) => {
     const text = ctx.message.text.split(' ').slice(1).join(' ');
@@ -7,7 +9,6 @@ module.exports = (bot) => {
       const apiURL = `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(text)}&isVideo=false`;
       const res = await axios.get(apiURL, { responseType: 'arraybuffer' });
 
-      // Kirim sebagai stiker dari buffer
       await ctx.replyWithSticker({ source: Buffer.from(res.data) });
     } catch (e) {
       console.error('Error saat membuat stiker:', e);
